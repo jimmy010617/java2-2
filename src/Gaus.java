b12c75d103a01579aa5a041fb7a10da5de2371ba
jimmy010617 <jimmy990411@email.daelim.ac.kr>
2024-03-29 15:47:13 +0900

    계산

---
+++ src/Gaus.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Gaus {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("숫자를 입력하세요 : ");
+        int foo = scanner.nextInt();
+        int bar = 0;
+
+        bar = ((foo + 1)*foo)/2;
+        System.out.println(bar);
+    }
+}
