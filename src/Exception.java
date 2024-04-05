1c945c353cdc409f9c7add7fb19b0df5b86422db
jimmy010617 <jimmy990411@email.daelim.ac.kr>
2024-04-05 14:48:39 +0900

    수업1

--- src/Exception.java
+++ src/Exception.java
@@ -7,17 +7,17 @@ public class Exception {
         int divisor;
 
         System.out.print("나뉨수를 입력하시오 : ");
-        divided = scanner.nextInt();
+        divided = scanner.nextInt(); //나뉨수 입력
         System.out.print("나눗수를 입력하시오 : ");
-        divisor = scanner.nextInt();
+        divisor = scanner.nextInt(); //나눗수 입력
         try {
             System.out.println(divided+"를 " + divisor + "로 나누면 몫은 " + divided/divisor + "입니다.");
         }
-        catch(ArithmeticException e){
+        catch(ArithmeticException e){   //ArithmeticException예외처리코드
             System.out.println("0으로 나눌 수 없습니다!");
         }
         finally {
-            scanner.close();
+            scanner.close();    //정상적이든 예외가 발생하든 최종적으로 scanner를 닫는다
         }
     }
 }
