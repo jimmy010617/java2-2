ae09f5b31f2ca4c7f121fbcb4aa0a170cc2d3d74
jimmy010617 <jimmy990411@email.daelim.ac.kr>
2024-04-05 15:44:33 +0900

    강의7

--- src/Pizza.java
+++ src/Pizza.java
@@ -17,5 +17,11 @@ public class Pizza {
     public static void main(String[] args) {
         Pizza pizza = new Pizza(10, "자바피자");
         double area = pizza.getArea();
+        System.out.println(pizza.name + "의 면적은 " + area);
+
+        Pizza donut = new Pizza();
+        donut.name = "도넛피자";
+        area = donut.getArea();
+        System.out.println(donut.name + "의 면적은 " + area);
     }
 }
