2517a9f36f0f4267141cb72a64cfdc323f0bf6ce
jimmy010617 <jimmy990411@email.daelim.ac.kr>
2024-05-17 16:07:20 +0900

    수정6

--- src/FlowLayoutEx.java
+++ src/FlowLayoutEx.java
@@ -6,7 +6,9 @@ public class FlowLayoutEx extends JFrame {
         setTitle("FlowLayout 예제");                    //프레임 타이틀 달기
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container contentPane = getContentPane();             //컨텐트팬 알아내기
-            
+         
+        //왼쪽 정렬로, 수평간격을 30, 수직 간격을 40 픽셀로 배치
+        //FlowLayout 생성 
         contentPane.setLayout(new FlowLayout(FlowLayout.LEFT, 30, 40));              //컨텐트팬에 FlowLatout 배치관리자 달기
 
         contentPane.add(new JButton("add"));              
@@ -15,7 +17,7 @@ public class FlowLayoutEx extends JFrame {
         contentPane.add(new JButton("div")); 
         contentPane.add(new JButton("Calculate"));         
 
-        setSize(300, 150);                       //프레임 크기 300x150 설정
+        setSize(300, 200);                       //프레임 크기 300x200 설정
         setVisible(true);                                   //화면에 프레임 출력
     }
 
